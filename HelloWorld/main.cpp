dd475f163ee5a6f1d6e1491911ed9128ce6023a9
Carlos S. del Castillo <carlos@del-castillo.net>
2017-07-04 17:24:47 -0400

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Carlos S. del Castillo on 7/4/17.
+//  Copyright © 2017 Carlos S. del Castillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
